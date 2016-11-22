fb9ad2b250ab17859e51283296087ac465303eff
Callie Bensel <CallieBensel@Callies-MacBook-Pro.local>
2016-11-22 13:57:22 -0600

    fixed bug

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ function longerString(string1, string2) {
   //compare strings
   if(string1.length > string2.length){
     alert(string1 + " is longest");
-    return string1;
-  }else{
+  } else if (string1.length < string2.length){
     alert(string2 + " is longest");
-    return string2;
+  } else {
+    alert("The Strings are the same length!");
   }
+
   // bug: what if they are the same length?
 }
